0a68ff95519f197d68b9c7880eae8afec535300d
Federico Bolanos <federico@benuccilab.net>
2020-02-26 13:08:59 +0900

    Testing analog input reads.

--- main.py
+++ main.py
@@ -5,15 +5,25 @@
 
 import nidaqmx as ni
 import numpy as np
+from time import sleep, time
 
 
-data = np.array([0,1,1,0,1,0,1,0], dtype=np.uint8)
 
-system = ni.system.System.local()
 
-for device in system.devices:
-    print(device)
+task = ni.Task()
+task.ai_channels.add_ai_voltage_chan("Dev1/ai1:7")
+task.timing.cfg_samp_clk_timing(1000)
+task.start()
 
+sleep(5)
+
+data = task.read()
+
+
+print(data)
+print(type(data))
+
+task.close()
 # task = ni.Task()
 # task.ai_channels.add_ai_voltage_chan("Dev0/ai1:7")
 # task.CreateDOChan("/TestDevice/port0/line2","",PyDAQmx.DAQmx_Val_ChanForAllLines)
